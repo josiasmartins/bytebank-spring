b065e29b0d8af6e07474fb62cf89393a2cfe80b0
Caitano <josiasmartinscaitano@gmail.com>
2022-10-17 18:53:14 -0300

    feat: inital project java

--- src/main/java/com/bytebank/byebankspring/ByebankSpringApplication.java
+++ src/main/java/com/bytebank/byebankspring/ByebankSpringApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ByebankSpringApplication {
 
+	// main: métedo principal que vai fazer o boot (rodar)
 	public static void main(String[] args) {
 		SpringApplication.run(ByebankSpringApplication.class, args);
 	}
