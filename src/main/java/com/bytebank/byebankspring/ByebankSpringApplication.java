5230975a263a8509ad886596a0bb0923938d1d16
Caitano <josiasmartinscaitano@gmail.com>
2022-10-17 18:33:10 -0300

    feat: inital project java

---
+++ src/main/java/com/bytebank/byebankspring/ByebankSpringApplication.java
@@ -0,0 +1,13 @@
+package com.bytebank.byebankspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByebankSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByebankSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bytebank/byebankspring/ByebankSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytebank.byebankspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByebankSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
